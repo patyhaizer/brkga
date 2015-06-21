38bc4ccea3adfdfb8bc40592a1b677342350e876
Patricia Haizer <paty.haizer@gmail.com>
2015-06-21 11:52:38 -0300

    Making function to evaluate individuals

--- BRKGA.cu
+++ BRKGA.cu
@@ -11,8 +11,8 @@
 #include <cuda_runtime.h>
 #include <curand.h>
 #include <curand_kernel.h>
-
-#define tam_pop = 32
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
 
 #define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
     printf("Error at %s:%d\n",__FILE__,__LINE__);\
@@ -24,23 +24,135 @@
 using namespace std;
 
 struct Instance {
-    char m_alphabet[ALPHABET_SIZE];
-    char m_instanceData[INSTANCE_SIZE][CHROMOSSOME_SIZE];
+    char m_alphabet[ALPHABET_SIZE + 1];
+    char m_instanceData[INSTANCE_SIZE][CHROMOSSOME_SIZE + 1];
 };
 
 struct Individual {
     double m_chromossome[CHROMOSSOME_SIZE];
-    double m_fitness;
+    //double m_hammingDistances[INSTANCE_SIZE];
+    int m_fitness;
 };
 
 struct Population {
-    Individual m_chromossomes[BRKGA_pop_size][CHROMOSSOME_SIZE];
+    Individual m_chromossomes[BRKGA_pop_size];
     unsigned m_size;
 };
 
+///////////////////////////////// CUDA FUNCTIONS - BEGIN //////////////////////////////////////////////////////
+__device__
+char decodeAlele(const double& randomValue) {
+    // char returnValue;
+    // if (randomValue <= 0.50) {
+    //     int t = (int) floor(((randomValue * 100) - 5) / 5);
+    //     returnValue = t;
+    // }
+    // return returnValue;
+    if(randomValue <= 0.05){
+        return '0';
+    } else if(randomValue <= 0.10) {
+        return '1';
+    } else if(randomValue <= 0.15) {
+        return '2';
+    } else if(randomValue <= 0.20) {
+        return '3';
+    } else if(randomValue <= 0.25) {
+        return '4';
+    } else if(randomValue <= 0.30) {
+        return '5';
+    } else if(randomValue <= 0.35) {
+        return '6';
+    } else if(randomValue <= 0.40) {
+        return '7';
+    } else if(randomValue <= 0.45) {
+        return '8';
+    } else if(randomValue <= 0.50) {
+        return '9';
+    } else if(randomValue <= 0.55) {
+        return 'a';
+    } else if(randomValue <= 0.60) {
+        return 'b';
+    } else if(randomValue <= 0.65) {
+        return 'c';
+    } else if(randomValue <= 0.70) {
+        return 'd';
+    } else if(randomValue <= 0.75) {
+        return 'e';
+    } else if(randomValue <= 0.80) {
+        return 'f';
+    } else if(randomValue <= 0.85) {
+        return 'g';
+    } else if(randomValue <= 0.90) {
+        return 'h';
+    } else if(randomValue <= 0.95) {
+        return 'i';
+    } else {
+        return 'j';
+    }
+}
+
+__global__
+void setupRand(curandState * state, unsigned long seed) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    curand_init(seed, tid, 0, &state[tid]);
+}
+
+__global__ void genInitialPop(curandState* globalState, Population *pop) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int individualId = blockIdx.x;
+    int aleloId = threadIdx.x;
+    curandState localState = globalState[tid];
+    double random = curand_uniform_double(&localState);
+    if (aleloId < CHROMOSSOME_SIZE) {
+        pop->m_chromossomes[individualId].m_chromossome[aleloId] = random;
+        pop->m_chromossomes[individualId].m_fitness = 0;
+    }
+    globalState[tid] = localState;
+}
+
+__global__ void evaluate(Population *pop, Instance * inst) {
+    int individualId = blockIdx.x;
+    int aleloId = threadIdx.x;
+    int instanceId = blockIdx.y;
+    char decodedAlele;
+    __shared__ int chromossomeCost[CHROMOSSOME_SIZE];
+    __shared__ int individualFitness;
+
+    if (aleloId < CHROMOSSOME_SIZE) {
+        // DO I NEED INITIALIZATION HERE?????
+        chromossomeCost[aleloId] = 0;
+        __syncthreads();
+        decodedAlele = decodeAlele(pop->m_chromossomes[individualId].m_chromossome[aleloId]);
+        chromossomeCost[aleloId] = (decodedAlele != inst->m_instanceData[instanceId][aleloId]);
+        __syncthreads();
+        if (threadIdx.x == 0) {
+            individualFitness = 0;
+            // Sum all elements to calculate the cost
+            for (int i = 0; i < CHROMOSSOME_SIZE; ++i) {
+                individualFitness += chromossomeCost[i];
+            }
+            // Access global memory to do an atomic operation to uptade chromossome cost
+            // pop->m_chromossomes[individualId].m_hammingDistances[instanceId] = individualFitness;
+            atomicMax(&(pop->m_chromossomes[individualId].m_fitness), individualFitness);
+        }
+    }
+}
+
+// __global__ void updateFitness(Population *pop) {
+//     int individualId = blockIdx.x;
+//     int aleloId = threadIdx.x;
+
+//     if (threadIdx.x == 0) {
+
+//     }
+// }
+
+///////////////////////////////// CUDA FUNCTIONS - END ///////////////////////////////////////////////////////
+
+
 void loadFromFile(std::vector<string>& instanceData) {
     //COLOQUE AQUI O ARQUIVO QUE CONTÉM A INSTÂNCIA QUE PRETENDE EXECUTAR
-    string instance_dir = "/Users/PatriciaHaizer/Documents/Instance/20caracteres/";
+    string instance_dir = "/home/lapo/mfonseca/Documents/Patricia/Instance/20caracteres/";
     //string output_dir = "/Users/PatriciaHaizer/Documents/Instance/20caracteres/Result/";
 
     //ARQUIVOS DE INSTÂNCIAS A SEREM UTILIZADOS
@@ -74,7 +186,19 @@ void loadFromFile(std::vector<string>& instanceData) {
     delete [] filename;
 }
 
+void printInitialPopulation(Population * pop) {
+    for (int i = 0; i < BRKGA_pop_size; ++i) {
+        printf("Chromossome %d: ", i);
+        for (int j = 0; j< CHROMOSSOME_SIZE; ++j) {
+            printf("%.2f, ", pop->m_chromossomes[i].m_chromossome[j]);
+        }
+        printf("\n");
+    }
+}
+
 int main(int argc, char **argv) {
+    clock_t start,end;
+    start=clock();
     std::vector<string> tmpInstanceData;
     loadFromFile(tmpInstanceData);
     assert(tmpInstanceData.size() == INSTANCE_SIZE);
@@ -82,13 +206,49 @@ int main(int argc, char **argv) {
     char h_alphabet[ALPHABET_SIZE +1];
     strcpy(h_alphabet, "0123456789abcdefghij");
 
-    // convert string vector into char **
-    char instanceData[INSTANCE_SIZE][CHROMOSSOME_SIZE];
+    // convert string vector into char
+    char instanceData[INSTANCE_SIZE][CHROMOSSOME_SIZE + 1];
     for (int i = 0; i < INSTANCE_SIZE ; ++i) {
         strcpy(instanceData[i], tmpInstanceData[i].c_str());
     }
-    //const unsigned eliteSize = 0.25 * BRKGA_pop_size;  // number of elite items in the population
-    //const unsigned mutantsSize = 0.05 * BRKGA_pop_size;  // number of mutants introduced at each generation into the population
-    //const double rhoe = 0.70;  // probability that an offspring inherits the allele of its elite parent
+
+    // CPU variables:
+    Population * h_pop;
+    Instance * h_instance;
+    h_pop = (struct Population *)calloc(1,sizeof(struct Population));
+    h_instance = (struct Instance *)calloc(1,sizeof(struct Instance));
+    memcpy(h_instance->m_alphabet, h_alphabet, ALPHABET_SIZE+1);
+    memcpy(h_instance->m_instanceData, instanceData, INSTANCE_SIZE * (CHROMOSSOME_SIZE + 1) * sizeof(char));
+
+    // GPU variables
+    Population * d_pop;
+    curandState* devStates;
+    Instance * d_instance;
+
+    dim3 nblocks(NUM_BLOCKS,INSTANCE_SIZE);
+
+    CUDA_CALL(cudaMalloc((void **)&devStates, (NUM_BLOCKS * NUM_THREADS) *sizeof(curandState)));
+    CUDA_CALL(cudaMalloc((void **)&d_pop, sizeof(Population)));
+    CUDA_CALL(cudaMalloc((void **)&d_instance, sizeof(Instance)));
+
+    // copying CPU values to GPU values
+    CUDA_CALL(cudaMemcpy(d_instance, h_instance, sizeof(Instance), cudaMemcpyHostToDevice));
+
+    setupRand <<<NUM_BLOCKS, NUM_THREADS >>> (devStates, time(NULL));
+    genInitialPop <<<NUM_BLOCKS, NUM_THREADS>>> (devStates, d_pop);
+    evaluate <<<nblocks, NUM_THREADS>>>(d_pop, d_instance);
+    CUDA_CALL(cudaMemcpy(h_pop, d_pop, sizeof(Population), cudaMemcpyDeviceToHost));
+    DEBUG_BRKGA(printInitialPopulation(h_pop);)
+
+    // Free GPU memory
+    CUDA_CALL(cudaFree(d_pop));
+    CUDA_CALL(cudaFree(devStates));
+    CUDA_CALL(cudaFree(d_instance));
+    // Free CPU memory
+    free (h_pop);
+    free (h_instance);
+    end=clock();
+    double dif = ((double) (end - start)) / CLOCKS_PER_SEC ;
+    printf ("Your calculations took %.15lf seconds to run.\n", dif );
     return 0;
 }
--- configuration.h
+++ configuration.h
@@ -7,6 +7,13 @@
 #define CHROMOSSOME_SIZE 500
 #define INSTANCE_SIZE 10
 
+#define NUM_BLOCKS BRKGA_pop_size
+#define NUM_THREADS 500
+
 #define DEBUG_BRKGA(code) if (0) { code }
+
+//const unsigned eliteSize = 0.25 * BRKGA_pop_size;  // number of elite items in the population
+//const unsigned mutantsSize = 0.05 * BRKGA_pop_size;  // number of mutants introduced at each generation into the population
+//const double rhoe = 0.70;  // probability that an offspring inherits the allele of its elite parent
 
 #endif // CONFIGURATION_H
